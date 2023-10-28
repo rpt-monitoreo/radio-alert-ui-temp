cdff3493509cf8f5fde46f1de4bd99b5e5793902
Juan Baquero <juan.baquero.z@outlook.com>
2023-10-28 15:13:28 -0500

    Added anntd and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Button, DatePicker, Space } from 'antd';
 
+const { RangePicker } = DatePicker;
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,9 +20,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
+        <Button type="primary">Button</Button>
+        <Space direction="vertical" size={12}>
+          <RangePicker />
+          <RangePicker showTime />
+          <RangePicker picker="week" />
+          <RangePicker picker="month" />
+          <RangePicker picker="quarter" />
+          <RangePicker picker="year" />
+        </Space>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
