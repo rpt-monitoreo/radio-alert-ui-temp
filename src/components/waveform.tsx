915c28063f0fe89c6d2e80cd7876be31d46c84c9
Juan Baquero <juan.baquero.z@outlook.com>
2023-11-03 00:06:09 -0500

    Added dobleClick

--- src/components/waveform.tsx
+++ src/components/waveform.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef } from 'react';
-import WaveSurfer, { WaveSurferEvents } from 'wavesurfer.js';
-import RegionsPlugin, { Region } from 'wavesurfer.js/dist/plugins/regions.js';
+import WaveSurfer from 'wavesurfer.js';
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js';
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.js';
+import Minimap from 'wavesurfer.js/dist/plugins/minimap.js';
 
 interface WaveformProps {
   url: string;
@@ -31,7 +32,6 @@ const Waveform: React.FC<WaveformProps> = ({ url, onSelection }) => {
         minPxPerSec: 2, //El ancho del audio muestra o no la barra
 
         plugins: [
-          RegionsPlugin.create(),
           TimelinePlugin.create({
             height: 24,
             insertPosition: 'beforebegin',
@@ -43,21 +43,49 @@ const Waveform: React.FC<WaveformProps> = ({ url, onSelection }) => {
               color: '#2D5B88',
             },
           }),
+          Minimap.create({
+            container: waveformRef.current,
+            height: 20,
+            waveColor: '#ddd',
+            progressColor: '#999',
+            // the Minimap takes all the same options as the WaveSurfer itself
+          }),
         ],
       } as any);
 
-      wavesurfer.on('click', () => {
-        wavesurfer.play();
+      const wsRegions = wavesurfer.registerPlugin(RegionsPlugin.create());
+      wavesurfer.on('decode', () => {
+        // Markers (zero-length regions)
+        wsRegions.addRegion({
+          start: 19,
+          content: '1',
+          color: 'green',
+        });
+        wsRegions.addRegion({
+          start: 20,
+          content: '2',
+          color: 'red',
+        });
+        wsRegions.getRegions()[0].setOptions({ start: 120 });
       });
+      let touchtime = 0;
+      wavesurfer.on('click', () => {
+        if (touchtime == 0) {
+          // set first click
+          touchtime = new Date().getTime();
+        } else {
+          // compare first click to this click and see if they occurred within double click threshold
+          if (new Date().getTime() - touchtime < 800) {
+            // double click occurred
 
-      /* wavesurfer.on(
-        'region-created' as keyof WaveSurferEvents,
-        (region: Region) => {
-          region.on('update-end', () => {
-            onSelection(region.start, region.end);
-          });
-        },
-      ); */
+            wavesurfer.play();
+            touchtime = 0;
+          } else {
+            // not a double click so set as a new first click
+            touchtime = new Date().getTime();
+          }
+        }
+      });
 
       return () => wavesurfer.destroy();
     }
